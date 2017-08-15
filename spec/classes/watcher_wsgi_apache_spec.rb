9d2a0fb19836dda6a2c3b7ef7701064abe21c4c5
ZhongShengping <chdzsp@163.com>
2017-08-15 10:16:22 +0800

    Add WSGI support for `watcher-api'
    Adds support to run watcher-api with mod-wsgi[0].
    
    [0]https://review.openstack.org/#/c/450740/
    
    Change-Id: Ie6bbde98bee19ccc4a938bba28b66b759ccd3307
    Implements: blueprint wsgi-support

---
+++ spec/classes/watcher_wsgi_apache_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe 'watcher::wsgi::apache' do
+
+  shared_examples_for 'apache serving watcher with mod_wsgi' do
+    context 'with default parameters' do
+      it { is_expected.to contain_class('watcher::params') }
+      it { is_expected.to contain_class('apache') }
+      it { is_expected.to contain_class('apache::mod::wsgi') }
+      it { is_expected.to contain_class('apache::mod::ssl') }
+      it { is_expected.to contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :bind_port           => 9322,
+        :group               => 'watcher',
+        :path                => '/',
+        :servername          => facts[:fqdn],
+        :ssl                 => true,
+        :threads             => facts[:os_workers],
+        :user                => 'watcher',
+        :workers             => 1,
+        :wsgi_daemon_process => 'watcher',
+        :wsgi_process_group  => 'watcher',
+        :wsgi_script_dir     => platform_params[:wsgi_script_path],
+        :wsgi_script_file    => 'app',
+        :wsgi_script_source  => platform_params[:wsgi_script_source],
+      )}
+    end
+
+    context 'when overriding parameters using different ports' do
+      let :params do
+        {
+          :servername                => 'dummy.host',
+          :bind_host                 => '10.42.51.1',
+          :port                      => 12345,
+          :ssl                       => false,
+          :wsgi_process_display_name => 'watcher',
+          :workers                   => 37,
+        }
+      end
+      it { is_expected.to contain_class('watcher::params') }
+      it { is_expected.to contain_class('apache') }
+      it { is_expected.to contain_class('apache::mod::wsgi') }
+      it { is_expected.to_not contain_class('apache::mod::ssl') }
+      it { is_expected.to contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :bind_host                 => '10.42.51.1',
+        :bind_port                 => 12345,
+        :group                     => 'watcher',
+        :path                      => '/',
+        :servername                => 'dummy.host',
+        :ssl                       => false,
+        :threads                   => facts[:os_workers],
+        :user                      => 'watcher',
+        :workers                   => 37,
+        :wsgi_daemon_process       => 'watcher',
+        :wsgi_process_display_name => 'watcher',
+        :wsgi_process_group        => 'watcher',
+        :wsgi_script_dir           => platform_params[:wsgi_script_path],
+        :wsgi_script_file          => 'app',
+        :wsgi_script_source        => platform_params[:wsgi_script_source],
+      )}
+    end
+  end
+
+  on_supported_os({
+    :supported_os   => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts({
+          :os_workers     => 42,
+          :concat_basedir => '/var/lib/puppet/concat',
+          :fqdn           => 'some.host.tld',
+        }))
+      end
+
+      let(:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          {
+            :wsgi_script_path   => '/usr/lib/cgi-bin/watcher',
+            :wsgi_script_source => '/usr/lib/python2.7/dist-packages/watcher/api/app.wsgi'
+          }
+        when 'RedHat'
+          {
+            :wsgi_script_path   => '/var/www/cgi-bin/watcher',
+            :wsgi_script_source => '/usr/lib/python2.7/site-packages/watcher/api/app.wsgi'
+          }
+        end
+      end
+
+      it_behaves_like 'apache serving watcher with mod_wsgi'
+    end
+  end
+end
