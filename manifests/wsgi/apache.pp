331bf7326a7975d130f16798acd7f50727c63256
ZhongShengping <chdzsp@163.com>
2017-10-23 14:13:37 +0800

    add parameter to overwrite/add wsgi process options
    Add parameter to apache_wsgi to allow overwrite
    and/or add additional wsgi process options.
    
    This possibility was added to openstacklib
    with Change-Id: I41914ce3361988d5db1695f09d21209772fdf548
    lease enter the commit message for your changes. Lines starting
    
    Change-Id: Ieab1cfea6bbf026b783c701e7631fb38c21061c4

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -78,6 +78,14 @@
 #     apache::vhost ssl parameters.
 #     Optional. Default to apache::vhost 'ssl_*' defaults.
 #
+# [*custom_wsgi_process_options*]
+#   (optional) gives you the oportunity to add custom process options or to
+#   overwrite the default options for the WSGI main process.
+#   eg. to use a virtual python environment for the WSGI process
+#   you could set it to:
+#   { python-path => '/my/python/virtualenv' }
+#   Defaults to {}
+#
 # == Dependencies
 #
 #   requires Class['apache'] & Class['watcher']
@@ -89,25 +97,26 @@
 #   class { 'watcher::wsgi::apache': }
 #
 class watcher::wsgi::apache (
-  $servername                 = $::fqdn,
-  $port                       = 9322,
-  $bind_host                  = undef,
-  $path                       = '/',
-  $ssl                        = true,
-  $workers                    = 1,
-  $ssl_cert                   = undef,
-  $ssl_key                    = undef,
-  $ssl_chain                  = undef,
-  $ssl_ca                     = undef,
-  $ssl_crl_path               = undef,
-  $ssl_crl                    = undef,
-  $ssl_certs_dir              = undef,
-  $wsgi_process_display_name  = undef,
-  $threads                    = $::os_workers,
-  $priority                   = '10',
-  $access_log_file            = false,
-  $access_log_format          = false,
-  $error_log_file             = undef,
+  $servername                  = $::fqdn,
+  $port                        = 9322,
+  $bind_host                   = undef,
+  $path                        = '/',
+  $ssl                         = true,
+  $workers                     = 1,
+  $ssl_cert                    = undef,
+  $ssl_key                     = undef,
+  $ssl_chain                   = undef,
+  $ssl_ca                      = undef,
+  $ssl_crl_path                = undef,
+  $ssl_crl                     = undef,
+  $ssl_certs_dir               = undef,
+  $wsgi_process_display_name   = undef,
+  $threads                     = $::os_workers,
+  $priority                    = '10',
+  $access_log_file             = false,
+  $access_log_format           = false,
+  $error_log_file              = undef,
+  $custom_wsgi_process_options = {},
 ) {
 
   include ::watcher::deps
@@ -119,32 +128,33 @@ class watcher::wsgi::apache (
   }
 
   ::openstacklib::wsgi::apache { 'watcher_wsgi':
-    bind_host                 => $bind_host,
-    bind_port                 => $port,
-    group                     => 'watcher',
-    path                      => $path,
-    priority                  => $priority,
-    servername                => $servername,
-    ssl                       => $ssl,
-    ssl_ca                    => $ssl_ca,
-    ssl_cert                  => $ssl_cert,
-    ssl_certs_dir             => $ssl_certs_dir,
-    ssl_chain                 => $ssl_chain,
-    ssl_crl                   => $ssl_crl,
-    ssl_crl_path              => $ssl_crl_path,
-    ssl_key                   => $ssl_key,
-    threads                   => $threads,
-    user                      => 'watcher',
-    workers                   => $workers,
-    wsgi_daemon_process       => 'watcher',
-    wsgi_process_display_name => $wsgi_process_display_name,
-    wsgi_process_group        => 'watcher',
-    wsgi_script_dir           => $::watcher::params::watcher_wsgi_script_path,
-    wsgi_script_file          => 'app',
-    wsgi_script_source        => $::watcher::params::watcher_wsgi_script_source,
-    require                   => Anchor['watcher::install::end'],
-    access_log_file           => $access_log_file,
-    access_log_format         => $access_log_format,
-    error_log_file            => $error_log_file,
+    bind_host                   => $bind_host,
+    bind_port                   => $port,
+    group                       => 'watcher',
+    path                        => $path,
+    priority                    => $priority,
+    servername                  => $servername,
+    ssl                         => $ssl,
+    ssl_ca                      => $ssl_ca,
+    ssl_cert                    => $ssl_cert,
+    ssl_certs_dir               => $ssl_certs_dir,
+    ssl_chain                   => $ssl_chain,
+    ssl_crl                     => $ssl_crl,
+    ssl_crl_path                => $ssl_crl_path,
+    ssl_key                     => $ssl_key,
+    threads                     => $threads,
+    user                        => 'watcher',
+    workers                     => $workers,
+    wsgi_daemon_process         => 'watcher',
+    wsgi_process_display_name   => $wsgi_process_display_name,
+    wsgi_process_group          => 'watcher',
+    wsgi_script_dir             => $::watcher::params::watcher_wsgi_script_path,
+    wsgi_script_file            => 'app',
+    wsgi_script_source          => $::watcher::params::watcher_wsgi_script_source,
+    require                     => Anchor['watcher::install::end'],
+    access_log_file             => $access_log_file,
+    access_log_format           => $access_log_format,
+    error_log_file              => $error_log_file,
+    custom_wsgi_process_options => $custom_wsgi_process_options,
   }
 }
--- spec/classes/watcher_wsgi_apache_spec.rb
+++ spec/classes/watcher_wsgi_apache_spec.rb
@@ -9,36 +9,40 @@ describe 'watcher::wsgi::apache' do
       it { is_expected.to contain_class('apache::mod::wsgi') }
       it { is_expected.to contain_class('apache::mod::ssl') }
       it { is_expected.to contain_openstacklib__wsgi__apache('watcher_wsgi').with(
-        :bind_port           => 9322,
-        :group               => 'watcher',
-        :path                => '/',
-        :servername          => facts[:fqdn],
-        :ssl                 => true,
-        :threads             => facts[:os_workers],
-        :user                => 'watcher',
-        :workers             => 1,
-        :wsgi_daemon_process => 'watcher',
-        :wsgi_process_group  => 'watcher',
-        :wsgi_script_dir     => platform_params[:wsgi_script_path],
-        :wsgi_script_file    => 'app',
-        :wsgi_script_source  => platform_params[:wsgi_script_source],
-        :access_log_file     => false,
-        :access_log_format   => false,
+        :bind_port                   => 9322,
+        :group                       => 'watcher',
+        :path                        => '/',
+        :servername                  => facts[:fqdn],
+        :ssl                         => true,
+        :threads                     => facts[:os_workers],
+        :user                        => 'watcher',
+        :workers                     => 1,
+        :wsgi_daemon_process         => 'watcher',
+        :wsgi_process_group          => 'watcher',
+        :wsgi_script_dir             => platform_params[:wsgi_script_path],
+        :wsgi_script_file            => 'app',
+        :wsgi_script_source          => platform_params[:wsgi_script_source],
+        :access_log_file             => false,
+        :access_log_format           => false,
+        :custom_wsgi_process_options => {},
       )}
     end
 
     context 'when overriding parameters using different ports' do
       let :params do
         {
-          :servername                => 'dummy.host',
-          :bind_host                 => '10.42.51.1',
-          :port                      => 12345,
-          :ssl                       => false,
-          :wsgi_process_display_name => 'watcher',
-          :workers                   => 37,
-          :access_log_file           => '/var/log/httpd/access_log',
-          :access_log_format         => 'some format',
-          :error_log_file            => '/var/log/httpd/error_log'
+          :servername                  => 'dummy.host',
+          :bind_host                   => '10.42.51.1',
+          :port                        => 12345,
+          :ssl                         => false,
+          :wsgi_process_display_name   => 'watcher',
+          :workers                     => 37,
+          :access_log_file             => '/var/log/httpd/access_log',
+          :access_log_format           => 'some format',
+          :error_log_file              => '/var/log/httpd/error_log',
+          :custom_wsgi_process_options => {
+            'python_path' => '/my/python/path',
+          },
         }
       end
       it { is_expected.to contain_class('watcher::params') }
@@ -46,24 +50,27 @@ describe 'watcher::wsgi::apache' do
       it { is_expected.to contain_class('apache::mod::wsgi') }
       it { is_expected.to_not contain_class('apache::mod::ssl') }
       it { is_expected.to contain_openstacklib__wsgi__apache('watcher_wsgi').with(
-        :bind_host                 => '10.42.51.1',
-        :bind_port                 => 12345,
-        :group                     => 'watcher',
-        :path                      => '/',
-        :servername                => 'dummy.host',
-        :ssl                       => false,
-        :threads                   => facts[:os_workers],
-        :user                      => 'watcher',
-        :workers                   => 37,
-        :wsgi_daemon_process       => 'watcher',
-        :wsgi_process_display_name => 'watcher',
-        :wsgi_process_group        => 'watcher',
-        :wsgi_script_dir           => platform_params[:wsgi_script_path],
-        :wsgi_script_file          => 'app',
-        :wsgi_script_source        => platform_params[:wsgi_script_source],
-        :access_log_file           => '/var/log/httpd/access_log',
-        :access_log_format         => 'some format',
-        :error_log_file            => '/var/log/httpd/error_log'
+        :bind_host                   => '10.42.51.1',
+        :bind_port                   => 12345,
+        :group                       => 'watcher',
+        :path                        => '/',
+        :servername                  => 'dummy.host',
+        :ssl                         => false,
+        :threads                     => facts[:os_workers],
+        :user                        => 'watcher',
+        :workers                     => 37,
+        :wsgi_daemon_process         => 'watcher',
+        :wsgi_process_display_name   => 'watcher',
+        :wsgi_process_group          => 'watcher',
+        :wsgi_script_dir             => platform_params[:wsgi_script_path],
+        :wsgi_script_file            => 'app',
+        :wsgi_script_source          => platform_params[:wsgi_script_source],
+        :access_log_file             => '/var/log/httpd/access_log',
+        :access_log_format           => 'some format',
+        :error_log_file              => '/var/log/httpd/error_log',
+        :custom_wsgi_process_options => {
+          'python_path' => '/my/python/path',
+        },
       )}
     end
   end
