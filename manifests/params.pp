23c9bfbae11378761fc6e190ff65baaa487af84a
zhulingjie <easyzlj@gmail.com>
2018-09-13 18:47:46 -0400

    Fix the duplicated vars defined
    Change-Id: I57511901fc9a2ac48473fd53dfd756868f1ae2c9
    Closes-Bug: #1792705
    (cherry picked from commit ed34f375858ec723f2fa9aedbf2d6f1cb9a563a4)

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,13 +2,15 @@
 #
 class watcher::params {
   include ::openstacklib::defaults
-  $group = 'watcher'
 
   if ($::os_package_type == 'debian') {
     $pyvers = '3'
   } else {
     $pyvers = ''
   }
+
+  $client_package_name = "python${pyvers}-watcherclient"
+  $group               = 'watcher'
 
   case $::osfamily {
     'RedHat': {
@@ -19,7 +21,6 @@ class watcher::params {
       $applier_service_name         = 'openstack-watcher-applier'
       $decision_engine_package_name = 'openstack-watcher-decision-engine'
       $decision_engine_service_name = 'openstack-watcher-decision-engine'
-      $client_package_name          = undef
       $watcher_wsgi_script_source   = '/usr/lib/python2.7/site-packages/watcher/api/app.wsgi'
       $watcher_wsgi_script_path     = '/var/www/cgi-bin/watcher'
     }
@@ -31,7 +32,6 @@ class watcher::params {
       $applier_service_name         = 'watcher-applier'
       $decision_engine_package_name = 'watcher-decision-engine'
       $decision_engine_service_name = 'watcher-decision-engine'
-      $client_package_name          = "python${pyvers}-watcherclient"
       if ($::os_package_type == 'debian') {
         $watcher_wsgi_script_source = '/usr/share/watcher-common/app.wsgi'
       } else {
