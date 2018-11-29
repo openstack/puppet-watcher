3ee52840342a696c5738950ae86dbe8582d27548
Tobias Urdin <tobias.urdin@binero.se>
2018-11-29 11:08:55 +0100

    Dont include logging in init class by default
    Change-Id: If5fc3e4e22226ccc97f03afb4dc0dc9a84e4fba4

--- manifests/init.pp
+++ manifests/init.pp
@@ -369,7 +369,6 @@ class watcher (
   include ::watcher::params
   include ::watcher::policy
   include ::watcher::db
-  include ::watcher::logging
 
   package { 'watcher':
     ensure => $package_ensure,
