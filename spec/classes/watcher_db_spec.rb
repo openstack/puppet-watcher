231dde81ad349c72652b6afa54157cecf84d6632
Tobias Urdin <tobias.urdin@binero.se>
2019-01-18 11:02:58 +0100

    Remove redundantly tested code
    Remove code that is redundantly tested.
    
    This should not be tested here but in puppet-oslo
    where this logic resides.
    
    If we keep this and we do changes in puppet-oslo we
    will break these unit tests, this is something we need
    to sort out for all modules.
    
    Change-Id: I2f6e69dec7e1f7431f9768462f8c1d2816217bc0

--- spec/classes/watcher_db_spec.rb
+++ spec/classes/watcher_db_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe 'watcher::db' do
-
   shared_examples 'watcher::db' do
     context 'with default parameters' do
-      it { is_expected.to contain_oslo__db('watcher_config').with(
+      it { should contain_class('watcher::deps') }
+
+      it { should contain_oslo__db('watcher_config').with(
         :db_max_retries => '<SERVICE DEFAULT>',
         :connection     => 'sqlite:////var/lib/watcher/watcher.sqlite',
         :idle_timeout   => '<SERVICE DEFAULT>',
@@ -19,7 +20,8 @@ describe 'watcher::db' do
 
     context 'with specific parameters' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://watcher:watcher@localhost/watcher',
+        {
+          :database_connection     => 'mysql+pymysql://watcher:watcher@localhost/watcher',
           :database_idle_timeout   => '3601',
           :database_min_pool_size  => '2',
           :database_db_max_retries => '-1',
@@ -31,7 +33,9 @@ describe 'watcher::db' do
         }
       end
 
-      it { is_expected.to contain_oslo__db('watcher_config').with(
+      it { should contain_class('watcher::deps') }
+
+      it { should contain_oslo__db('watcher_config').with(
         :db_max_retries => '-1',
         :connection     => 'mysql+pymysql://watcher:watcher@localhost/watcher',
         :idle_timeout   => '3601',
@@ -43,67 +47,6 @@ describe 'watcher::db' do
         :pool_timeout   => '21',
       )}
     end
-
-    context 'with postgresql backend' do
-      let :params do
-        { :database_connection => 'postgresql://watcher:watcher@localhost/watcher', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-psycopg2').with(:ensure => 'present')
-      end
-
-    end
-
-    context 'with MySQL-python library as backend package' do
-      let :params do
-        { :database_connection => 'mysql://watcher:watcher@localhost/watcher', }
-      end
-
-      it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
-    end
-
-    context 'with incorrect database_connection string' do
-      let :params do
-        { :database_connection => 'foodb://watcher:watcher@localhost/watcher', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-    context 'with incorrect pymysql database_connection string' do
-      let :params do
-        { :database_connection => 'foo+pymysql://watcher:watcher@localhost/watcher', }
-      end
-
-      it_raises 'a Puppet::Error', /validate_re/
-    end
-
-  end
-
-  shared_examples_for 'watcher::db on Debian' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://watcher:watcher@localhost/watcher', }
-      end
-
-      it 'install the proper backend package' do
-        is_expected.to contain_package('python-pymysql').with(
-          :ensure => 'present',
-          :name   => 'python-pymysql',
-          :tag    => 'openstack'
-        )
-      end
-    end
-  end
-
-  shared_examples_for 'watcher::db on RedHat' do
-    context 'using pymysql driver' do
-      let :params do
-        { :database_connection => 'mysql+pymysql://watcher:watcher@localhost/watcher', }
-      end
-
-    end
   end
 
   on_supported_os({
@@ -114,8 +57,7 @@ describe 'watcher::db' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_configures 'watcher::db'
-      it_configures "watcher::db on #{facts[:osfamily]}"
+      it_behaves_like 'watcher::db'
     end
   end
 end
