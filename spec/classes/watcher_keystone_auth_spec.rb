b35b585fa5b72c597813ad3d3a330ea20ad2c9e0
Takashi Kajinami <tkajinam@redhat.com>
2022-01-24 15:08:59 +0900

    Do not test detail of keystone::auth::service_identity
    ... but test interfaces of that resource type. This helps us avoid
    direct breakage caused by any change in puppet-keystone.
    
    Change-Id: I26343e1f146ebd55e2bcda2419bf1195ceea632a

--- spec/classes/watcher_keystone_auth_spec.rb
+++ spec/classes/watcher_keystone_auth_spec.rb
@@ -5,112 +5,64 @@
 require 'spec_helper'
 
 describe 'watcher::keystone::auth' do
-  shared_examples_for 'watcher-keystone-auth' do
+  shared_examples_for 'watcher::keystone::auth' do
     context 'with default class parameters' do
       let :params do
-        { :password => 'watcher_password',
-          :tenant   => 'foobar' }
+        { :password => 'watcher_password' }
       end
 
-      it { is_expected.to contain_keystone_user('watcher').with(
-        :ensure   => 'present',
-        :password => 'watcher_password',
-      ) }
-
-      it { is_expected.to contain_keystone_user_role('watcher@foobar').with(
-        :ensure  => 'present',
-        :roles   => ['admin']
-      )}
-
-      it { is_expected.to contain_keystone_service('watcher::infra-optim').with(
-        :ensure      => 'present',
-        :description => 'Infrastructure Optimization service'
-      ) }
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/watcher::infra-optim').with(
-        :ensure       => 'present',
-        :public_url   => 'http://127.0.0.1:9322',
-        :admin_url    => 'http://127.0.0.1:9322',
-        :internal_url => 'http://127.0.0.1:9322',
+      it { is_expected.to contain_keystone__resource__service_identity('watcher').with(
+        :configure_user      => true,
+        :configure_user_role => true,
+        :configure_endpoint  => true,
+        :service_name        => 'watcher',
+        :service_type        => 'infra-optim',
+        :service_description => 'Infrastructure Optimization service',
+        :region              => 'RegionOne',
+        :auth_name           => 'watcher',
+        :password            => 'watcher_password',
+        :email               => 'watcher@localhost',
+        :tenant              => 'services',
+        :public_url          => 'http://127.0.0.1:9322',
+        :internal_url        => 'http://127.0.0.1:9322',
+        :admin_url           => 'http://127.0.0.1:9322',
       ) }
     end
 
-    context 'when overriding URL parameters' do
+    context 'when overriding parameters' do
       let :params do
-        { :password     => 'watcher_password',
-          :public_url   => 'https://10.10.10.10:80',
-          :internal_url => 'http://10.10.10.11:81',
-          :admin_url    => 'http://10.10.10.12:81', }
-      end
-
-      it { is_expected.to contain_keystone_endpoint('RegionOne/watcher::infra-optim').with(
-        :ensure       => 'present',
-        :public_url   => 'https://10.10.10.10:80',
-        :internal_url => 'http://10.10.10.11:81',
-        :admin_url    => 'http://10.10.10.12:81',
-      ) }
-    end
-
-    context 'when overriding auth name' do
-      let :params do
-        { :password => 'foo',
-          :auth_name => 'watchery' }
-      end
-
-      it { is_expected.to contain_keystone_user('watchery') }
-      it { is_expected.to contain_keystone_user_role('watchery@services') }
-      it { is_expected.to contain_keystone_service('watchery::infra-optim') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/watchery::infra-optim') }
-    end
-
-    context 'when overriding service name' do
-      let :params do
-        { :service_name => 'watcher_service',
-          :auth_name    => 'watcher',
-          :password     => 'watcher_password' }
-      end
-
-      it { is_expected.to contain_keystone_user('watcher') }
-      it { is_expected.to contain_keystone_user_role('watcher@services') }
-      it { is_expected.to contain_keystone_service('watcher_service::infra-optim') }
-      it { is_expected.to contain_keystone_endpoint('RegionOne/watcher_service::infra-optim') }
-    end
-
-    context 'when disabling user configuration' do
-
-      let :params do
-        {
-          :password       => 'watcher_password',
-          :configure_user => false
-        }
-      end
-
-      it { is_expected.not_to contain_keystone_user('watcher') }
-      it { is_expected.to contain_keystone_user_role('watcher@services') }
-      it { is_expected.to contain_keystone_service('watcher::infra-optim').with(
-        :ensure      => 'present',
-        :description => 'Infrastructure Optimization service'
-      ) }
-
-    end
-
-    context 'when disabling user and user role configuration' do
-
-      let :params do
-        {
-          :password            => 'watcher_password',
+        { :password            => 'watcher_password',
+          :auth_name           => 'alt_watcher',
+          :email               => 'alt_watcher@alt_localhost',
+          :tenant              => 'alt_service',
+          :configure_endpoint  => false,
           :configure_user      => false,
-          :configure_user_role => false
-        }
+          :configure_user_role => false,
+          :service_description => 'Alternative Infrastructure Optimization service',
+          :service_name        => 'alt_service',
+          :service_type        => 'alt_infra-optim',
+          :region              => 'RegionTwo',
+          :public_url          => 'https://10.10.10.10:80',
+          :internal_url        => 'http://10.10.10.11:81',
+          :admin_url           => 'http://10.10.10.12:81' }
       end
 
-      it { is_expected.not_to contain_keystone_user('watcher') }
-      it { is_expected.not_to contain_keystone_user_role('watcher@services') }
-      it { is_expected.to contain_keystone_service('watcher::infra-optim').with(
-        :ensure      => 'present',
-        :description => 'Infrastructure Optimization service'
+      it { is_expected.to contain_keystone__resource__service_identity('watcher').with(
+        :configure_user      => false,
+        :configure_user_role => false,
+        :configure_endpoint  => false,
+        :service_name        => 'alt_service',
+        :service_type        => 'alt_infra-optim',
+        :service_description => 'Alternative Infrastructure Optimization service',
+        :region              => 'RegionTwo',
+        :auth_name           => 'alt_watcher',
+        :password            => 'watcher_password',
+        :email               => 'alt_watcher@alt_localhost',
+        :tenant              => 'alt_service',
+        :public_url          => 'https://10.10.10.10:80',
+        :internal_url        => 'http://10.10.10.11:81',
+        :admin_url           => 'http://10.10.10.12:81',
       ) }
-
     end
   end
 
@@ -122,7 +74,7 @@ describe 'watcher::keystone::auth' do
         facts.merge!(OSDefaults.get_facts())
       end
 
-      it_behaves_like 'watcher-keystone-auth'
+      it_behaves_like 'watcher::keystone::auth'
     end
   end
 end
