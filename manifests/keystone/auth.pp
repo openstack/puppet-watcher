9f113400cc69632d6eb43fdbc91399ed6fbf67c6
Takashi Kajinami <tkajinam@redhat.com>
2022-01-25 10:59:38 +0900

    Accept system scope credentials for Keystone API request
    This change is the first step to support secure RBAC and allows usage
    of system scope credentials for Keystone API request.
    
    This change covers the following two items.
     - assignment of system scope roles to system user
     - credential parameters for authtoken middleware
    
    Depends-on: https://review.opendev.org/804325
    Change-Id: I7675e58508a00979a3c26908c4449be24a98a206

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -19,6 +19,18 @@
 #   (Optional) Tenant for watcher user.
 #   Defaults to 'services'.
 #
+# [*roles*]
+#   (Optional) List of roles assigned to watcher user.
+#   Defaults to ['admin']
+#
+# [*system_scope*]
+#   (Optional) Scope for system operations.
+#   Defaults to 'all'
+#
+# [*system_roles*]
+#   (Optional) List of system roles assigned to watcher user.
+#   Defaults to []
+#
 # [*configure_endpoint*]
 #   (Optional) Should watcher endpoint be configured?
 #   Defaults to true.
@@ -67,6 +79,9 @@ class watcher::keystone::auth (
   $auth_name           = 'watcher',
   $email               = 'watcher@localhost',
   $tenant              = 'services',
+  $roles               = ['admin'],
+  $system_scope        = 'all',
+  $system_roles        = [],
   $configure_endpoint  = true,
   $configure_user      = true,
   $configure_user_role = true,
@@ -103,6 +118,9 @@ class watcher::keystone::auth (
     password            => $password,
     email               => $email,
     tenant              => $tenant,
+    roles               => $roles,
+    system_scope        => $system_scope,
+    system_roles        => $system_roles,
     public_url          => $public_url,
     internal_url        => $internal_url,
     admin_url           => $admin_url,
