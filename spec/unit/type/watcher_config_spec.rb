00ef4a99f1c0dbc91472934271cd3921aaca3bc9
Takashi Kajinami <tkajinam@redhat.com>
2022-02-13 00:14:04 +0900

    Add support for MultiStrOpt
    This replaces the provider implementation of watcher_config type so
    that MultiStrOpt, which is used by several options like
     - oslo_messaging_notifications/driver
     - oslo_policy/policy_dirs
    are handled correctly.
    
    Change-Id: I3e92b0123cf18803cc8abfebcbe48b51812a8c27

--- spec/unit/type/watcher_config_spec.rb
+++ spec/unit/type/watcher_config_spec.rb
@@ -30,12 +30,12 @@ describe 'Puppet::Type.type(:watcher_config)' do
 
   it 'should accept a valid value' do
     @watcher_config[:value] = 'bar'
-    expect(@watcher_config[:value]).to eq('bar')
+    expect(@watcher_config[:value]).to eq(['bar'])
   end
 
   it 'should not accept a value with whitespace' do
     @watcher_config[:value] = 'b ar'
-    expect(@watcher_config[:value]).to eq('b ar')
+    expect(@watcher_config[:value]).to eq(['b ar'])
   end
 
   it 'should accept valid ensure values' do
