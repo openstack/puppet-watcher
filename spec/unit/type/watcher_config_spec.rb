2006eba593870296cf36ef7fec282bd70be6f27c
Takashi Kajinami <tkajinam@redhat.com>
2022-07-15 11:57:01 +0900

    Fix wrong test description
    The value with a white space should be accepted instead of being
    rejected. This change fixes the wrong test case description and makes
    sure the description explains what is actually tested.
    
    Change-Id: Ibe1a44f9e63c7c210955942d70c0f485c3883852

--- spec/unit/type/watcher_config_spec.rb
+++ spec/unit/type/watcher_config_spec.rb
@@ -33,7 +33,7 @@ describe 'Puppet::Type.type(:watcher_config)' do
     expect(@watcher_config[:value]).to eq(['bar'])
   end
 
-  it 'should not accept a value with whitespace' do
+  it 'should accept a value with whitespace' do
     @watcher_config[:value] = 'b ar'
     expect(@watcher_config[:value]).to eq(['b ar'])
   end
