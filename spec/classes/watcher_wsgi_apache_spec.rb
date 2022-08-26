4faa43474205a9b3787af5e969e407f8320bddb5
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 17:35:15 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: Iaf3e13cbda1cf5fe25f8c4b9fb945f13fa4f30f3

--- spec/classes/watcher_wsgi_apache_spec.rb
+++ spec/classes/watcher_wsgi_apache_spec.rb
@@ -22,9 +22,14 @@ describe 'watcher::wsgi::apache' do
         :wsgi_script_source          => platform_params[:wsgi_script_source],
         :headers                     => nil,
         :request_headers             => nil,
-        :access_log_file             => nil,
-        :access_log_format           => nil,
         :custom_wsgi_process_options => {},
+        :access_log_file             => nil,
+        :access_log_pipe             => nil,
+        :access_log_syslog           => nil,
+        :access_log_format           => nil,
+        :error_log_file              => nil,
+        :error_log_pipe              => nil,
+        :error_log_syslog            => nil,
       )}
     end
 
@@ -37,9 +42,6 @@ describe 'watcher::wsgi::apache' do
           :ssl                         => true,
           :wsgi_process_display_name   => 'watcher',
           :workers                     => 37,
-          :access_log_file             => '/var/log/httpd/access_log',
-          :access_log_format           => 'some format',
-          :error_log_file              => '/var/log/httpd/error_log',
           :custom_wsgi_process_options => {
             'python_path' => '/my/python/path',
           },
@@ -66,12 +68,73 @@ describe 'watcher::wsgi::apache' do
         :wsgi_script_source          => platform_params[:wsgi_script_source],
         :headers                     => ['set X-XSS-Protection "1; mode=block"'],
         :request_headers             => ['set Content-Type "application/json"'],
-        :access_log_file             => '/var/log/httpd/access_log',
-        :access_log_format           => 'some format',
-        :error_log_file              => '/var/log/httpd/error_log',
         :custom_wsgi_process_options => {
           'python_path' => '/my/python/path',
         },
+      )}
+    end
+
+    context 'with custom access logging' do
+      let :params do
+        {
+          :access_log_format => 'foo',
+          :access_log_syslog => 'syslog:local0',
+          :error_log_syslog  => 'syslog:local1',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :access_log_format => params[:access_log_format],
+        :access_log_syslog => params[:access_log_syslog],
+        :error_log_syslog  => params[:error_log_syslog],
+      )}
+    end
+
+    context 'with access_log_file' do
+      let :params do
+        {
+          :access_log_file => '/path/to/file',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :access_log_file => params[:access_log_file],
+      )}
+    end
+
+    context 'with access_log_pipe' do
+      let :params do
+        {
+          :access_log_pipe => 'pipe',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :access_log_pipe => params[:access_log_pipe],
+      )}
+    end
+
+    context 'with error_log_file' do
+      let :params do
+        {
+          :error_log_file => '/path/to/file',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :error_log_file => params[:error_log_file],
+      )}
+    end
+
+    context 'with error_log_pipe' do
+      let :params do
+        {
+          :error_log_pipe => 'pipe',
+        }
+      end
+
+      it { should contain_openstacklib__wsgi__apache('watcher_wsgi').with(
+        :error_log_pipe => params[:error_log_pipe],
       )}
     end
   end
