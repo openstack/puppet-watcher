012583523e7109be33642c234d9fb2acc3a8d647
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-10-12 12:21:28 +0900

    Expose configure_service
    This parameter has been implemented in the underlying defined resource
    type but was not configurable.
    
    Change-Id: I10cd07571763281eda7c5dafcd75ad63b12ea469

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -43,6 +43,10 @@
 #   (Optional) Should the admin role be configured for the service user?
 #   Defaults to true.
 #
+# [*configure_service*]
+#   (Optional) Should the service be configurd?
+#   Defaults to True
+#
 # [*service_type*]
 #   (Optional) Type of service.
 #   Defaults to 'infra-optim'.
@@ -85,6 +89,7 @@ class watcher::keystone::auth (
   Boolean $configure_endpoint             = true,
   Boolean $configure_user                 = true,
   Boolean $configure_user_role            = true,
+  Boolean $configure_service              = true,
   Optional[String[1]] $service_name       = undef,
   String[1] $service_description          = 'Infrastructure Optimization service',
   String[1] $service_type                 = 'infra-optim',
@@ -104,6 +109,7 @@ class watcher::keystone::auth (
     configure_user      => $configure_user,
     configure_user_role => $configure_user_role,
     configure_endpoint  => $configure_endpoint,
+    configure_service   => $configure_service,
     service_name        => $real_service_name,
     service_type        => $service_type,
     service_description => $service_description,
