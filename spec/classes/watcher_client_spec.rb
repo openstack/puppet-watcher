1d8a9852d8c173cdb355cb843b3f433da4e86f72
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-09-07 13:33:09 +0900

    Use new openstackclient tag
    The new openstackclient tag was added so that we can get all resources
    about openstack CLI more easily. This adds this tag to watcherclient
    because the package provides some sub-commands.
    
    Also make sure openstackclient is installed so that the 'openstack'
    command is available.
    
    Change-Id: Ie078debd7f75fa4c677a0e7c229a3e651035e2fc
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- spec/classes/watcher_client_spec.rb
+++ spec/classes/watcher_client_spec.rb
@@ -11,7 +11,7 @@ describe 'watcher::client' do
       is_expected.to contain_package('python-watcherclient').with(
         :ensure => 'present',
         :name   => platform_params[:client_package_name],
-        :tag    => 'openstack',
+        :tag    => ['openstack', 'openstackclient'],
       )
     end
   end
