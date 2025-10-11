c6c18365756cd2eed329937d83f82040ab1511b4
Takashi Kajinami <kajinamit@oss.nttdata.com>
2025-10-11 12:08:17 +0900

    Add service role by default
    This is the role created to be used by service users. At this moment
    several services/functionalities still need the admin role so is kept.
    
    Change-Id: Id85bfbd2e75d1729feb920487995973c0f3ae9ce
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- manifests/keystone/auth.pp
+++ manifests/keystone/auth.pp
@@ -21,7 +21,7 @@
 #
 # [*roles*]
 #   (Optional) List of roles assigned to watcher user.
-#   Defaults to ['admin']
+#   Defaults to ['admin', 'service']
 #
 # [*system_scope*]
 #   (Optional) Scope for system operations.
@@ -83,7 +83,7 @@ class watcher::keystone::auth (
   String[1] $auth_name                    = 'watcher',
   String[1] $email                        = 'watcher@localhost',
   String[1] $tenant                       = 'services',
-  Array[String[1]] $roles                 = ['admin'],
+  Array[String[1]] $roles                 = ['admin', 'service'],
   String[1] $system_scope                 = 'all',
   Array[String[1]] $system_roles          = [],
   Boolean $configure_endpoint             = true,
